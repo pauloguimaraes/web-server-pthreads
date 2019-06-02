d62f720e93ff5370af39e331724a16a16f7cf305
phfguima <phfguima@hotmail.com>
2019-06-02 16:21:18 -0300

    Removendo arquivos excluídos

--- ep/pool_de_threads.h
+++
@@ -1,9 +0,0 @@
-#define MAX_THREADS 100
-
-typedef void *pool_de_threads;
-typedef void (*envio)(void *);
-
-pool_de_threads cria_pool_de_threads(int numero_de_threads);
-void enviar(pool_de_threads thread, envio funcao_de_envio, void *argumentos);
-void encerra_pool(pool_de_threads pool_para_encerrar);
-void *trabalho_thread(pool_de_threads pool);
